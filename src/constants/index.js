7c8bdfc282685c1bf5191bd0e22e4c04e3533aeb
neil0904 <neil19800904@gmail.com>
2026-06-10 02:16:50 +0500

    updated index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,6 +9,7 @@ import project2 from '../assets/projects/project-2.jpg';
 import project3 from '../assets/projects/project-3.jpg';
 import project4 from '../assets/projects/project-4.jpg';
 
+    //  updation
 export const HERO_CONTENT = `I am a passionate Full Stack Developer with 5+ years of experience in web development. I’ve built eCommerce platforms, donation portals like PakistanGiving, job portals, and custom web apps using ASP.NET Core, C#, ReactJS, Angular 19, Bootstrap, and TailwindCSS. Skilled in frontend & backend development, database integration, and creating responsive, user-friendly designs.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
